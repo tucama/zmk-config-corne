5468881d99524aa48b533f5d041f9223cc32688b
Tucamar <xxtucamarxx@hotmail.com>
2023-10-10 09:17:01 -0300

    br keys

---
+++ config/keys_br.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+
+
+// Aliases
+#define BR_ACUT LBRC       // ´ (dead)
+#define BR_TILD QUOT       // ~ (dead)
+#define BR_DIAE S(BR_6)    // ¨ (dead)
+#define BR_GRV  S(BR_ACUT) // ` (dead)
+#define BR_CIRC S(BR_TILD) // ^ (dead)
+#define BR_CCED SCLN       // Ç
+#define DE_SS   MINS       // ß
